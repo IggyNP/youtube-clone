e6f575106a26abb6ff958ca1e8b9e35fd310d202
Iggynp <odissey0x@gmail.com>
2023-03-25 19:14:58 +0100

    structured components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
-import { bgcolor } from "@mui/system";
+
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Box sx={{ backgroundColor: "#000" }}>
-        Navbar
+        <Navbar />
         <Routes>
           <Route path="/" exact element={<Feed />} />
           <Route path="/video/:id" element={<VideoDetail />} />
