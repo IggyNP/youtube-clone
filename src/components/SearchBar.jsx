44723e661d3b9475b9830b7bf7301a83f9d6edb6
Iggynp <odissey0x@gmail.com>
2023-04-19 18:14:40 +0200

    SearchBar and SearchFeed working

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
-import Search from "@mui/icons-material/Search";
+import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const onHandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm("");
+    }
+  };
   return (
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={onHandleSubmit}
       sx={{
         borderRadius: 20,
         border: "1px solid #e3e3e3",
@@ -19,15 +31,15 @@ const SearchBar = () => {
       <input
         className="search-bar"
         placeholder="Search..."
-        value=""
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <IconButton
         type="submit"
         sx={{ p: "10px", color: "red" }}
         aria-label="search"
       >
-        <Search />
+        <SearchIcon />
       </IconButton>
     </Paper>
   );
