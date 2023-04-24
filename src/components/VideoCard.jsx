6fdf16ce499420099af2384605338cabaae0bb8a
Iggynp <odissey0x@gmail.com>
2023-04-24 16:10:15 +0200

    better sizing on VideoCard

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -19,16 +19,16 @@ const VideoCard = ({
   return (
     <Card
       sx={{
-        width: { md: 320, xs: "100%" },
+        width: { xs: "92.5vw", sm: 358, md: 320 },
         boxShadow: "none",
         borderRadius: 0,
       }}
     >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
-          image={snippet?.thumbnails?.high?.url}
+          image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
           alt={snippet?.title}
-          sx={{ width: 358, height: 180 }}
+          sx={{ width: { xs: "100%", sm: 358, md: 320 }, height: 180 }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: "#1e1e1e", height: 106 }}>
